977acd249a3d6f9bd55f688f08ed729f51409d52
Chris Thornburg <cjthornburg@thornburgsfamily.com>
2023-10-20 19:12:37 -0400

    add alphanumeric check and '00' cents check and add to total points

--- app.js
+++ app.js
@@ -7,6 +7,43 @@ app.use(express.json());
 
 
 app.post('/receipts/process', async (req, res) => {
+    let points = 0
+
+    console.log(req.body)
+
+    const {retailer, purchaseDate, purchaseTime, total, items} = req.body
+
+    if ((!retailer || !purchaseDate ||!purchaseTime ||!total || !items)){
+        res.json({"error": "data missing"})
+        return
+    }
+
+    if (items.length ===0) {
+        res.json({"error": "receipt has not purchases"})
+    }
+
+// One point for every alphanumeric character in the retailer name.
+const alphanumericArray = retailer.match(/[a-zA-Z0-9]/g);
+ points += alphanumericArray.length
+
+ console.log(points, "after alphanumeric count")
+
+// 50 points if the total is a round dollar amount with no cents.
+let cents = total.substring(total.length-2, total.length)
+if (cents === "00")  points+= 50
+console.log(points, "after 00 cents check")
+// if ()
+// 25 points if the total is a multiple of 0.25.
+
+// 5 points for every two items on the receipt.
+
+
+// If the trimmed length of the item description is a multiple of 3, multiply the price by 0.2 and round up to the nearest integer. The result is the number of points earned.
+
+// 6 points if the day in the purchase date is odd.
+
+// 10 points if the time of purchase is after 2:00pm and before 4:00pm.
+
 
     console.log("hi from in post")
 
