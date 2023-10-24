f73d36b746ade23e01a5bcd99a93494e216df12e
Chris Thornburg <cjthornburg@thornburgsfamily.com>
2023-10-23 20:53:26 -0400

    add id generator and update return json for post request

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const app = express();
+const { v4: uuidv4 } = require('uuid');
 
 
 app.use(express.json());
 
-
+const rewardsHistoryObj = {}
 
 app.post('/receipts/process', async (req, res) => {
     let points = 0
 
-    console.log(req.body)
+
 
     const {retailer, purchaseDate, purchaseTime, total, items} = req.body
 
@@ -26,18 +27,20 @@ app.post('/receipts/process', async (req, res) => {
 const alphanumericArray = retailer.match(/[a-zA-Z0-9]/g);
  points += alphanumericArray.length
 
- console.log(points, "after alphanumeric count")
+//  console.log(points, "after alphanumeric count")
 
 // 50 points if the total is a round dollar amount with no cents.
 const cents = total.substring(total.length-2, total.length)
 if (cents === "00")  points+= 50
-console.log(points, "after 00 cents check")
+
+// console.log(points, "after 00 cents check")
 
 // 25 points if the total is a multiple of 0.25.
 const centsNumber = parseInt(cents)
 const divisibleBy25 = centsNumber % 25
 if (divisibleBy25 === 0) points += 25
-console.log(points, "after divisible by 25 check")
+
+// console.log(points, "after divisible by 25 check")
 
 // 5 points for every two items on the receipt.
 const numberOfItemsPurchased = items.length
@@ -45,7 +48,7 @@ const numberOfItemsPurchased = items.length
 if (numberOfItemsPurchased % 2 === 0) points += ((numberOfItemsPurchased/2) *5)
 else (points += (( numberOfItemsPurchased -1 )/2) *5)
 
-console.log(points, "5 points per 2 items")
+// console.log(points, "5 points per 2 items")
 
 // If the trimmed length of the item description is a multiple of 3, multiply the price by 0.2 and round up to the nearest integer. The result is the number of points earned.
 for (const item of items) {
@@ -55,7 +58,7 @@ for (const item of items) {
 }
 
 
-console.log(points, "after item description length")
+// console.log(points, "after item description length")
 
 // 6 points if the day in the purchase date is odd.
 const day = parseInt( purchaseDate.substring(purchaseDate.length-2, purchaseDate.length))
@@ -72,15 +75,18 @@ time += (.01 *  parseInt( purchaseTime.substring(3, 5)))
 
 
 
-    console.log(points, "points after time check")
+    // console.log(points, "points after time check")
 
 
 
 
 // add Id and save to local storage
+const id = uuidv4();
 
-// return 
-    res.json({ hi: "hi" })
+rewardsHistoryObj[id] = points
+
+// return
+    res.json({ id: id })
 })
 
 
