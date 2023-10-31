53c315e91dc4aefcf6d2010470a6ce9deb78f175
Chris Thornburg <cjthornburg@thornburgsfamily.com>
2023-10-31 10:56:25 -0400

    fix points error

--- app.js
+++ app.js
@@ -23,11 +23,17 @@ app.post('/receipts/process', async (req, res) => {
         res.json({"error": "receipt has not purchases"})
     }
 
+
+
+    
 // One point for every alphanumeric character in the retailer name.
 const alphanumericArray = retailer.match(/[a-zA-Z0-9]/g);
  points += alphanumericArray.length
 
 //  console.log(points, "after alphanumeric count")
+
+
+
 
 // 50 points if the total is a round dollar amount with no cents.
 const cents = total.substring(total.length-2, total.length)
@@ -35,12 +41,16 @@ if (cents === "00")  points+= 50
 
 // console.log(points, "after 00 cents check")
 
+
+
 // 25 points if the total is a multiple of 0.25.
 const centsNumber = parseInt(cents)
 const divisibleBy25 = centsNumber % 25
 if (divisibleBy25 === 0) points += 25
-
 // console.log(points, "after divisible by 25 check")
+
+
+
 
 // 5 points for every two items on the receipt.
 const numberOfItemsPurchased = items.length
@@ -50,15 +60,20 @@ else (points += (( numberOfItemsPurchased -1 )/2) *5)
 
 // console.log(points, "5 points per 2 items")
 
+
+
+
 // If the trimmed length of the item description is a multiple of 3, multiply the price by 0.2 and round up to the nearest integer. The result is the number of points earned.
 for (const item of items) {
-    if (item.shortDescription.length % 3 === 0) {
-        points += Math.ceil(item.price)
+    let trimmedString = item.shortDescription.trim()
+    if (trimmedString.length % 3 === 0) {
+        points += Math.ceil(item.price * 0.2)
     }
 }
 
-
 // console.log(points, "after item description length")
+
+
 
 // 6 points if the day in the purchase date is odd.
 const day = parseInt( purchaseDate.substring(purchaseDate.length-2, purchaseDate.length))
